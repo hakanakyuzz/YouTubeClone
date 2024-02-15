546d551d0bfbd9fe5437fc55c7d579ccf575b30d
Hakan <legion61614@gmail.com>
2024-02-15 21:45:30 +0300

    second commit

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,9 @@
+import React from "react";
+import './Home.css'
+
+const Home = ()=> {
+    return (<>
+    </>)
+}
+
+export default Home
---
+++ src/pages/Video/Video.js
@@ -0,0 +1,9 @@
+import React from "react";
+import './Video.css'
+
+const Video = ()=> {
+    return (<>
+    </>)
+}
+
+export default Video
