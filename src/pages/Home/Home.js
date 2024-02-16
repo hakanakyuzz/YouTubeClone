1aa0316a7bdf51de41c4d6568cbcb8c3995fc026
Hakan <legion61614@gmail.com>
2024-02-16 15:52:08 +0300

    video page added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,14 @@
 import React from "react";
 import './Home.css'
 import Sidebar from "../../components/Sidebar/Sidebar";
+import Feed from "../../components/Feed/Feed";
 
 const Home = ({sidebar})=> {
     return (<>
         <Sidebar sidebar={sidebar}/>
+        <div className={`container ${sidebar?"":"large-container"}`}>
+            <Feed />
+        </div>
     </>)
 }
 
